46679570003c735cf9b548bdadad54869d233d35
Vjsla <91530692+Vjsla@users.noreply.github.com>
2022-12-29 12:00:56 +0700

    Add files via upload

---
+++ lib/utils/color_utils.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: non_constant_identifier_names, prefer_interpolation_to_compose_strings
+
+import 'package:flutter/material.dart';
+
+HexStringToColor(String hexColor) {
+  hexColor = hexColor.toUpperCase().replaceAll("#", "");
+  if (hexColor.length == 6) {
+    hexColor = "FF" + hexColor;
+  }
+  return Color(int.parse(hexColor, radix: 16));
+}
